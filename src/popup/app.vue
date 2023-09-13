0e1af8a44e1b6d69bd364f582857379d1b509402
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-14 00:12:04 +0300

    app

---
+++ src/popup/app.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!--
+  <header
+    aria-label="Site Header"
+    class="bg-gray-50"
+  >
+    Popup Header
+    <button class="bg-cyan-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+  </header>
+-->
+  <RouterView></RouterView>
+
+  <!--
+  <footer
+    aria-label="Site Footer"
+    class="bg-gray-50"
+  >
+    Popup Footer
+  </footer>
+  -->
+</template>
+
+<style scoped></style>
