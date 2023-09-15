ffca111f3a00ca16627b179927c5ca77107f5086
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-15 10:48:35 +0300

    accounts

---
+++ src/pages/accounts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-4">
+    <RouterLink to="/about">Import account</RouterLink>
+    <h1>List Accounts</h1>
+    <button @click="$router.back">Go Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accounts"
+}
+</script>
+
+<style scoped>
+
+</style>
